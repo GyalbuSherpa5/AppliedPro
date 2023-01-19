adf22594f0d0b70873cfd060263564ee8ec34e79
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-01-19 16:12:07 +0545

    rock paper scissors

---
+++ RockPaperScissors/gameon.py
@@ -0,0 +1,27 @@
+import random
+
+
+def play_game():
+    options = ["rock", "paper", "scissors"]
+    player_choice = input("Enter a choice (rock, paper, scissors): ").lower()
+    while player_choice not in options:
+        player_choice = input("Invalid choice. Enter a valid choice (rock, paper, scissors): ").lower()
+    computer_choice = random.choice(options)
+    print(f"Player : {player_choice}")
+    print(f"Computer : {computer_choice}")
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif (player_choice == "rock" and computer_choice == "scissors") or (
+            player_choice == "paper" and computer_choice == "rock") or (
+            player_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+    else:
+        print("You lose!")
+
+
+playing = True
+while playing:
+    play_game()
+    if not input("Play again? (y/n): ").lower() == "y":
+        playing = False
+print("Thanks for playing!")
